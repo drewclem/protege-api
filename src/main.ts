af8e74e8e82a08bfd76a6bca56ea77682aaaf2f6
ANullBoolean <bryandesigning@gmail.com>
2020-03-31 17:43:07 -0400

    Added swagger api docs

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
-declare const module: any;
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('Protege API')
+    .setDescription('The backend API for Protege')
+    .setVersion('1.0')
+    .addTag('protege')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 
-  if (module.hot) {
-    module.hot.accept();
-    module.hot.dispose(() => app.close());
-  }
 }
 bootstrap();
